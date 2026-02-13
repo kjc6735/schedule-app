26b4c81374ff0ada66205c456ba56551b367c2d5
kimjaechan <penguin_03@naver.com>
2026-02-13 23:02:38 +0900

    feat: CORS 활성화
    - src/main.ts: app.enableCors() 추가

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { PrismaExceptionFilter } from './common/filter/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
